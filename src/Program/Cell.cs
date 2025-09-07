f3ed9ab34ac185941656ce9c752e3058a2311986
Dolphin <ignacioalonsosclavo@gmail.com>
2025-09-07 18:16:13 -0300

    Se agrego clases Celula y Tablero
    Se agrego clases Celula con su constructor y clase Tablero

---
+++ src/Program/Cell.cs
@@ -0,0 +1,13 @@
+﻿namespace Ucu.Poo.GameOfLife;
+public class Cell
+{ //Atributos: 
+    private bool[,] _gameBoard; //Tablero
+   
+    //-------Constructor de la clase Cell------------
+    public Cell(bool[,] initialBoard) 
+    {
+        _gameBoard = initialBoard;  //Tablero inicial
+    }
+    //----------------------------------------------------
+  
+}
